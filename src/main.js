a9176b66c35e64908c8f01a6c0513a45aa2e4f5e
kz <rkdevmail@gmail.com>
2017-02-07 09:08:43 -0600

    Learned how to mount the vue object Basic input and list building component are working Basic delete list item function working Understand more about attaching vue object to correct document scope Linted original source js file for errors

--- src/main.js
+++ src/main.js
@@ -2,11 +2,27 @@
 import Vue from 'vue';
 require('../index.html');
 
-window.helloworld = new Vue(
-  {
-    el: '#helloworld',
-    data:{
-      helloworld: "Hello, World!"
+window.app = new Vue({
+
+  data: {
+    todos: [1,2,3],
+    newTodo: ''
+  },
+  watch:{},
+  computed:{},
+  filters:{},
+  methods:{
+    addTodo: function() {
+      this.todos.push(
+        this.newTodo
+      );
+      this.newTodo = '';
+    },
+    removeTodo: function (todo) {
+        this.todos.splice(this.todos.indexOf(todo),1);
     }
-  }
-);
+  },
+  directives:{}
+});
+
+app.$mount('#todoapp');
