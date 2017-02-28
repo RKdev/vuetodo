158ea6dcccf834f9464d322d495b56a400fc6308
kz <rkdevmail@gmail.com>
2017-02-27 19:45:59 -0600

    cleaned out comments, cleaned up html

--- src/main.js
+++ src/main.js
@@ -1,31 +1,25 @@
 /*jshint esversion:6*/     //tells linter to be quiet about node syntax
 import Vue from 'vue';
-require('../index.html'); //tells webpack hot loader to reset on html changes
-require('../index.css'); //tells webpack hot loader to reset on css changes
+require('../index.html');
+require('../index.css');
 
-// routes
 var routes = {
-  all: 'all',              // #/all
-  active: 'active',       // #/active
-  completed: 'completed' // #/completed
+  all: 'all',
+  active: 'active',
+  completed: 'completed'
 };
 
-//define router
 function onHashChange() {
-  var route = window.location.hash.replace(/#\/?/, ''); //strip out the # and /
-  if (routes[route]) {           //check routes list for valid route
-    app.currentRoute = route;   // set route
-                               //really it sets a string that gets passed to the "filters" object
+  var route = window.location.hash.replace(/#\/?/, '');
+  if (routes[route]) {
+    app.currentRoute = route;
   } else {
     window.location.hash = '';
     app.currentRoute = 'all';
   }
 }
 
-//instantiate router
 window.addEventListener('hashchange', onHashChange);
-
-//localStorage
 
 var STORAGE_KEY = 'todos-vuejs-2.1';
 var todoStorage = {
@@ -38,26 +32,15 @@ var todoStorage = {
   }
 };
 
-/*.filter is an arrayPrototype function.
-   filter receives an array of all todos, then returns a new array of
-   elements that pass the filter
-*/
-
-//I want a different word than "filters" for this part
-//it's easy to confuse with vue filters when I'm trying to reason about the code
-//it works for now though
 var filters = {
-  //no filter - returns everything
   all: function (todos) {
     return todos;
   },
-  //returns everything that hasn't been completed yet (completed items disappear when checked)
   active: function (todos) {
     return todos.filter(function (todo) {
       return !todo.completed;
     });
   },
-  //returns everything that has been completed
   completed: function (todos) {
     return todos.filter(function (todo) {
       return todo.completed;
@@ -70,8 +53,7 @@ window.app = new Vue({
   data: {
     todos: todoStorage.fetch(),
     newTodo: '',
-    currentRoute: 'all', //define this as a real route, or else vue will cough
-                       // when it can't find this value in the "filters" object
+    currentRoute: 'all',
     editedTodo: false,
     beforeEditCache: ''
   },
@@ -83,29 +65,19 @@ window.app = new Vue({
     }
   },
   computed:{
-    /* passes the current route to the "filters" object and runs
-       the associated function to return a filtered list of todo items
-    */
     filteredTodos: function () {
       return filters[this.currentRoute](this.todos);
     },
     allDone: {
-      //changes checkbox based on if there are remaining unfinished todos
       get: function(){
-          return this.remaining === 0; //boolean typecast based on if any todos are unfinished
+          return this.remaining === 0;
         },
-      //somehow passing an argument to the "set" function lets you see the value of "allDone"
-      //but the same doesn't apply to "get" for some reason
       set: function (value) {
         this.todos.forEach(function (todo) {
           todo.completed = value;
         });
       }
-      //if check box is set to true, sets all to true
-      //if check box is set to false, sets all to false
-      //check box is only true if all are true or none exist
     },
-    // returns number of remaining todos
     remaining: function () {
       return filters.active(this.todos).length;
     }
@@ -124,22 +96,17 @@ window.app = new Vue({
       this.newTodo = '';
     },
     removeTodo: function (todo) {
-        //look up index of selected todo in array of todos and remove it
         this.todos.splice(this.todos.indexOf(todo),1);
     },
     clearTodo: function(){
-      //use reactive property of vue data elements to clear the bound input box
       this.newTodo = '';
     },
     removeCompleted: function () {
-      //creates an array of the current active elements
-     // and overwrites the todos array
       this.todos = filters.active(this.todos);
     },
     editTodo: function (todo) {
       this.beforeEditCache = todo.title;
       this.editedTodo = todo;
-      console.log(todo.title);
     },
     cancelEdit: function (todo) {
       this.editedTodo = false;
@@ -159,11 +126,6 @@ window.app = new Vue({
   directives:{
     'todo-focus': function (el, expression){
       if (expression.value === true) {
-        el.focus();
-      }
-    },
-    'todo-input-focus': function (el, expression){
-      if (expression.value === false) {
         el.focus();
       }
     }
